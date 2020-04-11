0873f50189ddd5fe6c24a82151c37e3ea7d8a25f
Masami <hiracaka.edson@gmail.com>
2020-04-11 15:49:52 -0400

    Ajustes no sidebar

---
+++ TCC/Sisgef/Controllers/VeiculoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sisgef.Controllers
+{
+    public class VeiculoController : Controller
+    {
+        // GET: Veiculo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CadastrarVeiculo()
+        {
+            return View();
+        }
+    }
+}
